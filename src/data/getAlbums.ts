3dfbd00540584b4e76d1cff68970c7d3e4492060
joshuarichard <eos.josh.richard@gmail.com>
2025-06-10 14:29:45 -0400

    update airtable token

--- src/data/getAlbums.ts
+++ src/data/getAlbums.ts
@@ -2,11 +2,10 @@ import Airtable from 'airtable';
 import { Album } from '../types/Album';
 import * as config from '../config.json';
 
-const MUSIC_TIMELINE_AIRTABLE_API_KEY = config.MUSIC_TIMELINE_AIRTABLE_API_KEY;
-const MUSIC_TIMELINE_AIRTABLE_BASE_ID = config.MUSIC_TIMELINE_AIRTABLE_BASE_ID;
+const { MUSIC_TIMELINE_AIRTABLE_PERSONAL_ACCESS_TOKEN, MUSIC_TIMELINE_AIRTABLE_BASE_ID } = config;
 
 const base = new Airtable({
-  apiKey: MUSIC_TIMELINE_AIRTABLE_API_KEY,
+  apiKey: MUSIC_TIMELINE_AIRTABLE_PERSONAL_ACCESS_TOKEN,
 }).base(MUSIC_TIMELINE_AIRTABLE_BASE_ID || '');
 
 const getAlbums = () => {
